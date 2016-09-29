d3564c8170cf50bd18d0da30ec81d300a9d271a6
Brittany See <brittanyjsee@gmail.com>
2016-09-28 20:49:56 -0700

    add content

--- alphabet_html/js/main.js
+++ alphabet_html/js/main.js
@@ -11,21 +11,21 @@ jQuery(function ($) {
 
         //Google Map
 
-        var mapCanvas = document.getElementById('map-canvas');
-        var myLatlng = new google.maps.LatLng(60.391263, 5.322054); //your latitude and longitude here
-        var mapOptions = {
-            zoom: 16,
-            scrollwheel: false,
-            center: myLatlng,
-            styles:[{"featureType":"landscape","stylers":[{"hue":"#FFB000"},{"saturation":71.66666666666669},{"lightness":-28.400000000000006},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#E8FF00"},{"saturation":-76.6},{"lightness":113},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF8300"},{"saturation":-77},{"lightness":27.400000000000006},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF8C00"},{"saturation":-66.6},{"lightness":34.400000000000006},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#00C4FF"},{"saturation":22.799999999999997},{"lightness":-11.399999999999991},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#9FFF00"},{"saturation":0},{"lightness":-23.200000000000003},{"gamma":1}]}]
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            icon: 'img/mapmarker.png',
-            map: map,
-            title: ''
-        });
+        // var mapCanvas = document.getElementById('map-canvas');
+        // var myLatlng = new google.maps.LatLng(60.391263, 5.322054); //your latitude and longitude here
+        // var mapOptions = {
+        //     zoom: 16,
+        //     scrollwheel: false,
+        //     center: myLatlng,
+        //     styles:[{"featureType":"landscape","stylers":[{"hue":"#FFB000"},{"saturation":71.66666666666669},{"lightness":-28.400000000000006},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#E8FF00"},{"saturation":-76.6},{"lightness":113},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF8300"},{"saturation":-77},{"lightness":27.400000000000006},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF8C00"},{"saturation":-66.6},{"lightness":34.400000000000006},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#00C4FF"},{"saturation":22.799999999999997},{"lightness":-11.399999999999991},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#9FFF00"},{"saturation":0},{"lightness":-23.200000000000003},{"gamma":1}]}]
+        // }
+        // var map = new google.maps.Map(mapCanvas, mapOptions)
+        // var marker = new google.maps.Marker({
+        //     position: myLatlng,
+        //     icon: 'img/mapmarker.png',
+        //     map: map,
+        //     title: ''
+        // });
 		
 
         //Scrolling feature 
